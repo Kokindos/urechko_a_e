670abb9042b08ecf33a0a04ea2565c8be263564d
Kokindos <kokindoss@gmail.com>
2022-09-28 23:59:53 +0300

    add implementation

--- queue_r/queue_r.cpp
+++ queue_r/queue_r.cpp
@@ -4,39 +4,43 @@
 
 #include "queue_r.h"
 
-QueueR(const QueueR &copy){
-    Node* current=copy.head;
+QueueR::QueueR(const QueueR &copy) {
+    head = new Node(copy.head->value);
+    Node *currentCopy = copy.head->next;
+    Node *current = head;
+    while (currentCopy != nullptr) {
+        current->next = new Node(currentCopy->value);
+        currentCopy = currentCopy->next;
+    }
+}
+
+QueueR &QueueR::operator=(const QueueR &rhs) {
 
 };
 
-QueueR &operator=(const QueueR &rhs){
-
-};
-
-~QueueR(); //деструктор указателя уничтожает сам указатель, а объект на которым ссылается указатель не уничтожается
+QueueR::~QueueR() {}; //деструктор указателя уничтожает сам указатель, а объект на которым ссылается указатель не уничтожается
 void QueueR::push(int value) {
     if (head == nullptr) {
-        head = &Node(value);
-    } else if (head->next == nullptr) { // если value < head
-        Node inserted = Node(value);
-        if (value < head->value) {
-            head = &inserted;
-            head->next = current;
-        }
+        head = new Node(value);
+    } else if (value < head->value) {
+        // если value < head
+        Node *inserted = new Node(value);
+        inserted->next = head;
+        head = inserted;
     } else {
         Node *current = head;
         while (current->next != nullptr && current->next->value <= value) {
             current = current->next;
         } // двигаем current в цикле а присваивание после цикла
-        Node inserted = Node(value);
-        inserted.next = current->next;
-        current->next = &inserted;
+        Node *inserted = new Node(value);
+        inserted->next = current->next;
+        current->next = inserted;
         //-> одновременно разыменовывает и достает значение
     }
 }
 
 void QueueR::pop() {
-    head = head.next;
+    head = head->next;
 }
 
 bool QueueR::empty() const {
---
+++ queue_r/queue_r_test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by admin on 28.09.2022.
+int main() {};
+//
+
